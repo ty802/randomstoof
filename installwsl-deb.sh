75267dace751d2b66ebc473cc8b6834ebc41fc07
Kian amamoto <kianjyamamoto@gmail.com>
2019-12-04 22:24:31 -0600

    idk

--- installwsl-deb.sh
+++ installwsl-deb.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt -y upgrade && sudo apt install -y zsh git cmake make python python3 curl &&sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
+sudo apt update && sudo apt -y upgrade && sudo apt install -y zsh git cmake make python python3 curl &&sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&echo "exec zsh" | less -a ~/.bashrc && exit
