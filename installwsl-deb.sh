905ef6cb362d8c9537fb6a7c4291b51099beed8b
kian yamamoto <kianhyamamoto@gmail.com>
2019-12-04 21:49:16 -0600

    On branch master  Changes to be committed: 	new file:   installwsl-deb.sh

---
+++ installwsl-deb.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt -y upgrade && sudo apt install -y zsh git cmake make python python3 curl &&sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
